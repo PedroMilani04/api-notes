d96f4409919fe4cb400b034c88c803d78089df42
Pedro Henrique Milani Vagula <89487697+PedroMilani04@users.noreply.github.com>
2023-05-28 11:45:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ require('./config/dbconfig') // requer a config do banco
 app.use(cors()) // usa o cors
 const port = process.env.PORT || 3333
 
+app.get('/', (req, res) => {
+    return res.json({ message: 'Hello World' })
+})
+
 
 app.use(express.json()) // usa o express
 app.use(routes) // usa as rotas
